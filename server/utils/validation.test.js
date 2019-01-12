db60208c0f740bd0bca70528f0ba358018259154
Alexey Eremin <draber06@gmail.com>
2019-01-13 00:43:59 +0300

    Add Users class and updateUserList event

---
+++ server/utils/validation.test.js
@@ -0,0 +1,24 @@
+const expect = require('expect');
+const {
+    isRealString
+} = require('./validation');
+
+describe('isRealString', () => {
+    it('should reject not-string values', () => {
+        var str = 123;
+
+        expect(isRealString(str)).toBe(false);
+    });
+
+    it('should reject string with only spaces', () => {
+        var str = "  ";
+
+        expect(isRealString(str)).toBe(false);
+    });
+
+    it('should allow string with non-space characters', () => {
+        var str = " Alexey Eremin ";
+
+        expect(isRealString(str)).toBe(true);
+    });
+});
